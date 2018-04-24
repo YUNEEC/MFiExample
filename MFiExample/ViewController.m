b190fca98d71c6388ee53f5cf881f69dd8f575b0
Julian Oes <julian@oes.ch>
2018-04-24 13:26:39 -0400

    Added simple example xcodeproject

---
+++ MFiExample/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  MFiExample
+//
+//  Created by Julian Oes on 24.04.18.
+//  Copyright © 2018 Yuneec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello world");
+    
+    // TODO: connect MFi
+    
+    // TODO: subscribe to mavlink
+    
+    // TODO: printf mavlink
+    
+    // TODO: forward mavlink via UDP on localhost
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
