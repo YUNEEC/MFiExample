05dd270ba53e39a139356929654f16539abbd51c
Julian Oes <julian@oes.ch>
2018-04-26 16:34:22 -0400

    Added hearbeat sending and message counting

--- MFiExample/MFiConnectionStateAdapter.m
+++ MFiExample/MFiConnectionStateAdapter.m
@@ -5,17 +5,25 @@
 
 #import <YuneecDataTransferManager/YuneecDataTransferConnectionState.h>
 #import <YuneecDataTransferManager/YuneecDataTransferManager.h>
+#import <YuneecDataTransferManager/YuneecControllerDataTransfer.h>
 
-@interface MFiConnectionStateAdapter() <YuneecDataTransferConnectionStateDelegate>
+#import "c_library_v2/yuneec/mavlink.h"
+
+@interface MFiConnectionStateAdapter() <YuneecDataTransferConnectionStateDelegate, YuneecFlyingControllerDataTransferDelegate>
 
 @property (assign, nonatomic, readwrite) BOOL                       connected;
 @property (assign, nonatomic, readwrite) BOOL                       bIsBackground;
 @property (assign, nonatomic) YuneecDataTransferConnectionType      connectionType;
 @property (strong, nonatomic) YuneecDataTransferConnectionState     *connectionState;
 
+@property (strong, nonatomic) dispatch_source_t     heartbeatTimer;
+
 @end
 
 @implementation MFiConnectionStateAdapter
+
+int numMessagesReceived = 0;
+int numMessagesSent = 0;
 
 NSString * const kMFiConnectionStateNotification = @"MFiConnectionStateNotification";
 
@@ -43,11 +51,13 @@ NSString * const kMFiConnectionStateNotification = @"MFiConnectionStateNotificat
                                                  selector:@selector(applicationWillEnterForeground)
                                                      name:UIApplicationWillEnterForegroundNotification
                                                    object:nil];
+        [[YuneecDataTransferManager sharedInstance] controllerDataTransfer].flyingControllerDelegate = self;
     }
     return self;
 }
 
 - (void)dealloc {
+    [self stopHeartbeatTimer];
     [[NSNotificationCenter defaultCenter] removeObserver:self
                                                     name:UIApplicationDidEnterBackgroundNotification
                                                   object:nil];
@@ -97,7 +107,10 @@ NSString * const kMFiConnectionStateNotification = @"MFiConnectionStateNotificat
     NSDictionary* userInfo = @{@"MFiConnectionState": @(self.connected),
                                @"MFiConnectionType": @(self.connectionType),
                                @"BackgroundState": @(self.bIsBackground),
-                               @"DroneMonitorLost":@(self.bDroneMonitorLost)};
+                               @"DroneMonitorLost":@(self.bDroneMonitorLost),
+                               @"NumMessagesReceived":@(numMessagesReceived),
+                               @"NumMessagesSent":@(numMessagesSent)
+                               };
     return userInfo;
 }
 
@@ -107,7 +120,13 @@ NSString * const kMFiConnectionStateNotification = @"MFiConnectionStateNotificat
    changeConnectionType:(YuneecDataTransferConnectionType) connectionType
                fromType:(YuneecDataTransferConnectionType) fromType
 {
-
+    if (connectionType != fromType) {
+        if (connectionType == 2) {
+            [self stopHeartbeatTimer];
+        } else if (connectionType == 0) {
+            [self startHeartbeatTimer];
+        }
+    }
     self.connectionType = connectionType;
     [[YuneecDataTransferManager sharedInstance] setCurrentDataTransferType:connectionType];
     [self notifyConnectionStateUpdate];
@@ -137,5 +156,65 @@ NSString * const kMFiConnectionStateNotification = @"MFiConnectionStateNotificat
     }
     return _connectionState;
 }
+
+- (void)controllerDataTransfer:(YuneecControllerDataTransfer *) dataTransfer
+didReceiveFlyingControllerData:(NSData *) data {
+    numMessagesReceived++;
+    [self notifyConnectionStateUpdate];
+}
+
+- (void)sendHeartbeatPackage {
+    // Don't send if not connected (anymore).
+    if (self.connectionType != 0) {
+        return;
+    }
+    mavlink_message_t       message;
+    mavlink_heartbeat_t     heartbeat;
+    
+    heartbeat.custom_mode   = 0;
+    heartbeat.type          = MAV_TYPE_GCS;
+    heartbeat.autopilot     = MAV_AUTOPILOT_INVALID;
+    heartbeat.base_mode     = MAV_MODE_FLAG_MANUAL_INPUT_ENABLED|MAV_MODE_FLAG_SAFETY_ARMED;
+    heartbeat.system_status = MAV_STATE_ACTIVE;
+    heartbeat.mavlink_version= 3;
+    
+    uint16_t package_len = mavlink_msg_heartbeat_encode(5, 10, &message, &heartbeat);
+    
+    uint8_t *buf = (uint8_t *)malloc(package_len);
+    uint16_t ret = mavlink_msg_to_send_buffer(buf, &message);
+#pragma unused(ret)
+    
+    NSData *sendData = [[NSData alloc] initWithBytes:buf length:package_len];
+    [self sendControllerDataToTransfer:sendData];
+    free(buf);
+    numMessagesSent++;
+}
+
+- (void)sendControllerDataToTransfer:(NSData *) data {
+    [[[YuneecDataTransferManager sharedInstance] controllerDataTransfer] sendData:data];
+}
+
+- (void)startHeartbeatTimer {
+    if (self.heartbeatTimer != nil) {
+        return;
+    }
+    self.heartbeatTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0));
+    dispatch_source_set_timer(self.heartbeatTimer, DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC, 0.0);
+    
+    dispatch_source_set_event_handler(self.heartbeatTimer, ^{
+        [self sendHeartbeatPackage];
+        NSLog(@"Sending heartbeat");
+    });
+    
+    dispatch_resume(self.heartbeatTimer);
+}
+
+- (void)stopHeartbeatTimer {
+    if (self.heartbeatTimer != nil) {
+        dispatch_source_cancel(self.heartbeatTimer);
+        self.heartbeatTimer = nil;
+    }
+}
+
 
 @end
