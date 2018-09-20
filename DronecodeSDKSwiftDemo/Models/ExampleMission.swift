6807838e71bb8b204f9bf66c923df5b2cec6918d
Douglas Silva <silvadouglasw@gmail.com>
2018-09-20 13:03:43 -0700

    Change mission CameraPoints to be photos only.

--- DronecodeSDKSwiftDemo/Models/ExampleMission.swift
+++ DronecodeSDKSwiftDemo/Models/ExampleMission.swift
@@ -24,35 +24,35 @@ class ExampleMission {
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.398039859999997, longitudeDeg: 8.5455725400000002, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: CameraAction.startPhotoInterval)
+                let entry = MissionItem(latitudeDeg: 47.398039859999997, longitudeDeg: 8.5455725400000002, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: .takePhoto)// CameraAction.startPhotoInterval)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.398036222362471, longitudeDeg: 8.5450146439425509, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -30.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: CameraAction.stopPhotoInterval)
+                let entry = MissionItem(latitudeDeg: 47.398036222362471, longitudeDeg: 8.5450146439425509, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -30.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: .takePhoto)// CameraAction.stopPhotoInterval)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.397825620791885, longitudeDeg: 8.5450092830163271, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: CameraAction.startVideo)
+                let entry = MissionItem(latitudeDeg: 47.397825620791885, longitudeDeg: 8.5450092830163271, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: .takePhoto)// CameraAction.startVideo)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.398039859999997, longitudeDeg: 8.5455725400000002, relativeAltitudeM: 10.0, speedMPS: 2.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: -90.0, loiterTimeS: Float.nan, cameraAction: CameraAction.startPhotoInterval)
+                let entry = MissionItem(latitudeDeg: 47.398039859999997, longitudeDeg: 8.5455725400000002, relativeAltitudeM: 10.0, speedMPS: 2.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: -90.0, loiterTimeS: Float.nan, cameraAction: .takePhoto) //CameraAction.startPhotoInterval)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.398039859999997, longitudeDeg: 8.5455725400000002, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: -90.0, loiterTimeS: Float.nan, cameraAction: CameraAction.startPhotoInterval)
+                let entry = MissionItem(latitudeDeg: 47.398039859999997, longitudeDeg: 8.5455725400000002, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: -90.0, loiterTimeS: Float.nan, cameraAction: .takePhoto)// CameraAction.startPhotoInterval)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.398036222362471, longitudeDeg: 8.5450146439425509, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -30.0, gimbalYawDeg: 0, loiterTimeS: Float.nan, cameraAction: CameraAction.stopPhotoInterval)
+                let entry = MissionItem(latitudeDeg: 47.398036222362471, longitudeDeg: 8.5450146439425509, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -30.0, gimbalYawDeg: 0, loiterTimeS: Float.nan, cameraAction: .takePhoto)//CameraAction.stopPhotoInterval)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.397825620791885, longitudeDeg: 8.5450092830163271, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: -60.0, loiterTimeS: Float.nan, cameraAction: CameraAction.startVideo)
+                let entry = MissionItem(latitudeDeg: 47.397825620791885, longitudeDeg: 8.5450092830163271, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: -60.0, gimbalYawDeg: -60.0, loiterTimeS: Float.nan, cameraAction: .takePhoto)//CameraAction.startVideo)
                 missionItems.append(entry)
             }
             do {
-                let entry = MissionItem(latitudeDeg: 47.397832880000003, longitudeDeg: 8.5455939999999995, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: 0.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: CameraAction.stopVideo)
+                let entry = MissionItem(latitudeDeg: 47.397832880000003, longitudeDeg: 8.5455939999999995, relativeAltitudeM: 10.0, speedMPS: 10.0, isFlyThrough: true, gimbalPitchDeg: 0.0, gimbalYawDeg: 0.0, loiterTimeS: Float.nan, cameraAction: .takePhoto)// CameraAction.stopVideo)
                 missionItems.append(entry)
             }
         }
