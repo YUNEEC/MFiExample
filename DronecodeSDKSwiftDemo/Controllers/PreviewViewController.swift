7018db4d853d19079e371aa5d98dcdb9be048f42
Joe Zhu <cg.zhu@yuneec.com>
2019-02-28 10:55:13 +0800

    add display offset and size example

--- DronecodeSDKSwiftDemo/Controllers/PreviewViewController.swift
+++ DronecodeSDKSwiftDemo/Controllers/PreviewViewController.swift
@@ -19,7 +19,11 @@ class PreviewViewController: UIViewController {
         super.viewDidLoad()
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: .UIApplicationDidEnterBackground, object: nil)
-         notificationCenter.addObserver(self, selector: #selector(appWillEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(appWillEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(orientationChanged), name: .UIDeviceOrientationDidChange, object: nil)
+
+        self.setDisplayRect()
+
         self.startVideo()
     }
     
@@ -44,7 +48,21 @@ class PreviewViewController: UIViewController {
             self.videoStarted = false;
         }
     }
-        
+
+    func setDisplayRect() {
+        var rect = CGRect(
+            origin: CGPoint(x: 0, y: 0),
+            size: self.previewView.bounds.size
+        )
+        //example to change display size
+        if (UIDevice.current.orientation == .portrait) {
+            rect.size.height = rect.size.height/2
+            } else {
+            rect.size.width = rect.size.width/2
+        }
+        MFiAdapter.MFiPreviewViewAdapter.sharedInstance().updateDisplay(rect)
+    }
+
     @objc func appMovedToBackground() {
         print("App moved to background!")
         MFiAdapter.MFiPreviewViewAdapter.sharedInstance().stopVideo { (result) in
@@ -59,4 +77,7 @@ class PreviewViewController: UIViewController {
         self.startVideo()
     }
     
+    @objc func orientationChanged() {
+        self.setDisplayRect()
+    }
 }
